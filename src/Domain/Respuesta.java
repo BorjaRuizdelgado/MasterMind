f8d8ff9c54b527929ebe9eee2cf1f79c893f28c6
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2017-11-02 13:18:39 +0100

    Respuesta equals hecho

--- src/Domain/Respuesta.java
+++ src/Domain/Respuesta.java
@@ -20,6 +20,6 @@ public class Respuesta {
      * @return cierto o falso dependiendo de si son iguales
      */
     public Boolean equals(Respuesta respuestaComprobada){
-        return true;
+       return this.respuesta.contains(respuestaComprobada.respuesta);
     }
 }
