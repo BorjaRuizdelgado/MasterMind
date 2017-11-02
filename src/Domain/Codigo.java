c8c024c9838b12b9347e30e0464d1266adfa58c1
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2017-11-02 11:49:32 +0100

    Codigo + respuesta

--- src/Domain/Codigo.java
+++ src/Domain/Codigo.java
@@ -1,4 +1,16 @@
 package Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Codigo {
+    public List<Integer> codigo;
+
+    public Codigo(int size){
+        codigo = new ArrayList<Integer>(size);
+    }
+
+    public Respuesta corregir(Codigo codigoACorregir){
+        
+    }
 }
