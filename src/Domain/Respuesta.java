2ded775de592326db964d88271d64825f97729d8
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2017-11-03 13:31:35 +0100

    respuesta no funciona  pero PrubaTest si

--- src/Domain/Respuesta.java
+++ src/Domain/Respuesta.java
@@ -1,15 +1,18 @@
 package Domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Respuesta {
     public List<Integer> respuesta;
+    public int size = 4;
 
     public Respuesta(int size){
         respuesta = new ArrayList<Integer>(size);
+        this.size = size;
     }
     public Boolean equals(Respuesta respuestaComprobada){
-        return this.respuesta.containsAll(respuestaComprobada.respuesta);
+        return Collections.sort(this.respuesta).equals(Collections.sort(respuestaComprobada.respuesta));
     }
 }
