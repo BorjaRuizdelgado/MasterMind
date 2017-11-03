d9ff5aef9eebb33a64094c6ca3d7f39269b35053
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2017-11-03 14:05:32 +0100

    FilaTest acabado

--- src/Test/FilaTest.java
+++ src/Test/FilaTest.java
@@ -1,7 +1,9 @@
 package Test;
 
 
+import Domain.Codigo;
 import Domain.Fila;
+import Domain.Respuesta;
 
 import java.util.Scanner;
 
@@ -18,11 +20,11 @@ public class FilaTest {
                 while(true) {
                     System.out.println("INTRODUCE: 1 para ver que colores tiene la fila, 2 para ver las respuestas,\n" +
                             "3 para insertar colores, 4 para insertar una respuesta");
-                    num = in.nextInt();
-                    if (num == 1) imprimeColores();
-                    if (num == 2) imprimeRespuestas();
-                    if (num == 3) insertarColores();
-                    if (num == 4) insertarRespuesta();
+                    int numerito = in.nextInt();
+                    if (numerito == 1) imprimeColores();
+                    if (numerito == 2) imprimeRespuestas();
+                    if (numerito == 3) insertarColores(num);
+                    if (numerito == 4) insertarRespuesta(num);
                 }
             }
             else System.out.print("Tamaño no válido\n");
@@ -34,5 +36,29 @@ public class FilaTest {
     }
     private static void imprimeRespuestas(){
         System.out.println(fila.getRespuestas().respuesta);
+    }
+
+    private static void insertarColores(int num){
+        System.out.println("Intorduce una serie de colores en forma de numeros\n");
+        Codigo code = new Codigo(num);
+        Scanner s = new Scanner(System.in);
+        for(int i = 0; i < num; i++) {
+            int numeroAInsertar = s.nextInt();
+            code.codigo.add(numeroAInsertar);
+            System.out.println(i);
+        }
+        fila.setColores(code);
+    }
+
+    private static void insertarRespuesta(int num){
+        System.out.println("Intorduce una serie de colores en forma de numeros\n");
+        Respuesta code = new Respuesta(num);
+        Scanner s = new Scanner(System.in);
+        for(int i = 0; i < num; i++) {
+            int numeroAInsertar = s.nextInt();
+            code.respuesta.add(numeroAInsertar);
+            System.out.println(i);
+        }
+        fila.setRespuestas(code);
     }
 }
