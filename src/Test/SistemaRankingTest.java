a47db229ce5e10a23f9816062a263f97026d6f87
Omar <omar.anibal.garcia@est.fib.upc.edu>
2017-11-06 21:45:38 +0100

    SistemaRanking Test [PROCESO]

--- src/Test/SistemaRankingTest.java
+++ src/Test/SistemaRankingTest.java
@@ -3,6 +3,8 @@ package Test;
 
 import Domain.Codigo;
 import Domain.Respuesta;
+import Domain.SistemaRanking;
+import Util.Console;
 
 import java.util.Scanner;
 
@@ -16,45 +18,9 @@ import java.util.Scanner;
 
 public class SistemaRankingTest {
 
-    private static void iniciaCodigo (Codigo codigo, int num) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Introduce los números (del 1 al 6) del código uno por uno\n");
-        int code;
-        for (int i = 0; i < num; i++) {
-            code = in.nextInt();
-            while (code < 1 || code > 6){
-                System.out.print("Número no válido. Introduce uno del 1 al 6\n");
-                code = in.nextInt();
-            }
-            codigo.codigo.add(code);
-        }
-    }
-
-    private static void imprimeInfoCodigo (Codigo codigo) {
-        System.out.print("Este es tu código: "+codigo.codigo+"\n" +
-                "Y tiene este tamaño: "+codigo.size+"\n");
-    }
-
+    private static SistemaRanking sistemaRanking = SistemaRanking.getInstance();
     public static void main(String[] args) {
-        System.out.print("Aquí probamos la Clase Código\n");
-        while (true) {
-            System.out.print("Probaremos un nuevo código.\n" +
-                    "Por favor, introduce el tamaño del código deseado: 4 o 6\n");
-            Scanner in = new Scanner(System.in);
-            int num = in.nextInt();
-            if (num == 4 || num == 6) {
-                Codigo test = new Codigo(num);
-                iniciaCodigo(test, num);
-                imprimeInfoCodigo(test);
-                System.out.print("Ahora probaremos de comparar dos códigos.\n" +
-                        "Crearemos un nuevo código que funcionará como código secreto\n");
-                Codigo testSecreto = new Codigo(num);
-                iniciaCodigo(testSecreto, num);
-                imprimeInfoCodigo(testSecreto);
-                System.out.print("Obtendríamos esta respuesta: "+test.getRespuesta(testSecreto).respuesta+"\n" +
-                        "Los 8 son fichas negras y los 7 fichas blancas. Los 0 fichas vacías.\n");
-                System.out.print("Prueba acabada\n");
-            } else System.out.print("Tamaño no válido\n");
-        }
+        Scanner in = new Scanner(System.in);
+        Console.println("Aquí probamos la Clase SistemaRanking.");
     }
 }
