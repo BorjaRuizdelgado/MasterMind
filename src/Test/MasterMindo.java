e2599b73d7ea72d0a67c7c4e179c43d603ccdca1
Borja Ruizdelgado <borjafruizdelgado@gmail.com>
2017-11-07 11:04:19 +0100

    Mastermindo puntua

--- src/Test/MasterMindo.java
+++ src/Test/MasterMindo.java
@@ -1,9 +1,6 @@
 package Test;
 
-import Domain.Codigo;
-import Domain.Partida;
-import Domain.Respuesta;
-import Domain.Usuario;
+import Domain.*;
 
 import java.util.Scanner;
 
@@ -26,7 +23,10 @@ public class MasterMindo {
             else if(rol == 2) juegaCodeBreaker();
     }
 
+    /**Permite al usuario jugar como CodeBreaker.
+     */
     private static void juegaCodeBreaker() {
+        long totalTime = 0;
         print("Dame un nivel de dificultad: Facil, Medio y Dificil");
         Scanner scan = new Scanner(System.in);
         String diff = scan.next();
@@ -37,26 +37,33 @@ public class MasterMindo {
         while(pary.getNumeroFilaActual() != 15 && !pary.isGanado()){
             print("Introduce un código de tamaño " + pary.getNumColumnas() +" separado por espacios\n" +
                     "Formado por numeros del 1 al "+ pary.getNumColores());
-            Codigo code = new Codigo(pary.getNumColumnas());
-            for(int i = 0; i < pary.getNumColumnas();++i){
-                code.codigo.add(scan.nextInt());
-            }
-            System.out.println(code.codigo);
-            pary.setIntento(code);
-            pary.generaRespuesta();
+            long startTime = System.currentTimeMillis();
+
+            computarIntentoUsuario();
+
+            long endTime = System.currentTimeMillis();
+            totalTime = totalTime + (endTime - startTime);
+
             print("Has obtenido esta respuesta: " + pary.getUltimaRespuesta().toString());
-            System.out.println(pary.getCodigoSecreto().codigo); //delete
         }
         if (!pary.isGanado()) {
             print("Has perdido! Este era el código secreto:");
             System.out.println(pary.getCodigoSecreto().codigo);
         }
+
         else {
             print("Has ganado con "+pary.getNumeroFilaActual()+" intentos.");
         }
 
+        System.out.println(usr.getNombre() + " tu puntuación total es de: " + computarRanking(totalTime,pary.getDificultad(),pary.getNumeroFilaActual())  +
+                " una puntuación percecta es de 100, cuanto más te pases de ese numero,\n" +
+                "peor puntuacón.");
+
     }
 
+    /**
+     * Permite al usuario jugar como CodeMaker.
+     */
     private static void juegaCodeMaker(){
         print("Dame un nivel de dificultad: Facil, Medio y Dificil");
         Scanner scan = new Scanner(System.in);
@@ -76,6 +83,9 @@ public class MasterMindo {
         }
     }
 
+    /**
+     * El usuario puede poner un codigo secreto en el tablero
+     */
     private static void setCodigoSecreto(){
         print("Dame el codigo secreto de tamaño " + pary.getNumColumnas() +" separado por espacios\n" +
                 "Formado por numeros del 1 al "+ pary.getNumColores());
@@ -90,6 +100,9 @@ public class MasterMindo {
         print("EL JUEGO EMPIEZA\n");
     }
 
+    /**
+     * El usuario corrige a la maquina.
+     */
     private static void corrige(){
         Respuesta respuestaUsr = new Respuesta(pary.getNumColumnas());
         Codigo intento = pary.getSiguienteIntento();
@@ -110,11 +123,44 @@ public class MasterMindo {
         pary.setRespuesta(respuestaUsr);
     }
 
-
-
-
     private static void print (String message) {
         System.out.print(message+"\n");
+    }
+
+    /**
+     * Dado una dificultad, un tiempo total de usuario y el numero de intentos esta funcion genera una puntuación.
+     * @param tiempo Tiempo total del Usuario para resolver el Mastermind.
+     * @param dificltad Dificultad Escogida.
+     * @param numeroFila Numero de intentos empleados.
+     * @return Puntuación.
+     */
+    private static int computarRanking(float tiempo, String dificltad, int numeroFila){
+        float resultado = 0;
+
+        resultado += (15 - numeroFila)*777;
+        resultado += 1000000/(tiempo/1000);
+        switch(dificltad){
+            case "Medio":
+                resultado *= 1.10;
+                break;
+            case "Dificil":
+                resultado *= 1.20;
+        }
+        return (int)resultado;
+    }
+
+    /**
+     * Lee el intento del usuario y lo pone en el tablero.
+     */
+    private static void computarIntentoUsuario(){
+        Scanner scan = new Scanner(System.in);
+        Codigo code = new Codigo(pary.getNumColumnas());
+        for(int i = 0; i < pary.getNumColumnas();++i){
+            code.codigo.add(scan.nextInt());
+        }
+
+        pary.setIntento(code);
+        pary.generaRespuesta();
     }
 }
 
