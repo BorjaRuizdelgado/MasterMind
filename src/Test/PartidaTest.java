9a5900786f44ab9ae51e01adfdf3a9b506fc8ea7
Isabel Codina <isakody@gmail.com>
2017-11-07 19:42:23 +0100

    Añadidas funciones de PartidaTest

--- src/Test/PartidaTest.java
+++ src/Test/PartidaTest.java
@@ -1,63 +1,61 @@
 package Test;
 
+import Domain.Codigo;
+import Domain.Partida;
+
 import static org.junit.Assert.*;
 
 public class PartidaTest {
-    @org.junit.Before
+
+    private Partida testMaker;
+    private Partida testBreaker;
+    private Partida testFacil;
+    private Partida testMedio;
+    private Partida testDificil;
+
+    @org.junit.BeforeClass
     public void setUp() throws Exception {
+        testMaker = new Partida(true, "Dificil");
+        testBreaker = new Partida(false, "Dificil");
+        testFacil = new Partida(true, "Facil");
+        testMedio = new Partida(true, "Medio");
+        testDificil = new Partida(true, "Dificil");
+
+        testFacil.generaCodigoSecretoAleatorio();
+        testMedio.generaCodigoSecretoAleatorio();
+        testDificil.generaCodigoSecretoAleatorio();
+
     }
 
-    @org.junit.After
+    @org.junit.AfterClass
     public void tearDown() throws Exception {
     }
 
-
     @org.junit.Test
-    public void getId() throws Exception {
+    public void testIsRolMaker() throws Exception {
+        assertEquals("Partida Maker es rolMaker true", testMaker.isRolMaker(), true);
+        assertEquals("Partida Breaker es rolMaker false", testBreaker.isRolMaker(), false);
     }
 
     @org.junit.Test
-    public void setId() throws Exception {
+    public void testGetDificultad() throws Exception {
+        assertEquals("Partida Facil es Dificultad Facil", testFacil.getDificultad(), "Facil");
+        assertEquals("Partida Medio es Dificultad Medio", testMedio.getDificultad(), "Medio");
+        assertEquals("Partida Dificil es Dificultad Dificil", testDificil.getDificultad(), "Dificil");
     }
 
     @org.junit.Test
-    public void getTiempo() throws Exception {
+    public void testGetNumColores() throws Exception {
+        assertEquals("Partida Facil tiene 4 colores", testFacil.getNumColores(), 4);
+        assertEquals("Partida Medio tiene 6 colores", testMedio.getNumColores(), 6);
+        assertEquals("Partida Dificil tiene 6 colores", testDificil.getNumColores(), 6);
     }
 
     @org.junit.Test
-    public void setTiempo() throws Exception {
-    }
-
-    @org.junit.Test
-    public void isRolMaker() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setRolMaker() throws Exception {
-    }
-
-    @org.junit.Test
-    public void getDificultad() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setDificultad() throws Exception {
-    }
-
-    @org.junit.Test
-    public void getNumColores() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setNumColores() throws Exception {
-    }
-
-    @org.junit.Test
-    public void getNumColumnas() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setNumColumnas() throws Exception {
+    public void testGetNumColumnas() throws Exception {
+        assertEquals("Partida Facil tiene 4 columnas", testFacil.getNumColumnas(), 4);
+        assertEquals("Partida Medio tiene 4 columnas", testMedio.getNumColumnas(), 4);
+        assertEquals("Partida Dificil tiene 6 columnas", testDificil.getNumColumnas(), 6);
     }
 
     @org.junit.Test
@@ -65,15 +63,11 @@ public class PartidaTest {
     }
 
     @org.junit.Test
-    public void sumaTiempo() throws Exception {
+    public void testSumaTiempo() throws Exception {
     }
 
     @org.junit.Test
-    public void setCodigoSecreto() throws Exception {
-    }
-
-    @org.junit.Test
-    public void getCodigoSecreto() throws Exception {
+    public void testGetCodigoSecreto() throws Exception {
     }
 
     @org.junit.Test
