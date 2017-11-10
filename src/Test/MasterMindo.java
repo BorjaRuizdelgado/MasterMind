bd599ce5ad34f82dbb89a92e17d2dd847f1c82b7
Isabel Codina <isakody@gmail.com>
2017-11-10 13:38:52 +0100

    Añadido menú en MasterMindo

--- src/Test/MasterMindo.java
+++ src/Test/MasterMindo.java
@@ -1,6 +1,8 @@
 package Test;
 
 import Domain.*;
+import Domain.Excepciones.ExcepcionRespuestaIncorrecta;
+
 import static Util.Console.*;
 import java.util.Scanner;
 
@@ -12,10 +14,9 @@ import java.util.Scanner;
 public class MasterMindo {
     public static Usuario usr;
     public static Partida pary;
+    private static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-            Scanner scan = new Scanner(System.in);
             println("Bienvenid@ a MasterMindo un juego completamente nuevo y \n" +
                     "que no infringe ningun tipo de derechos de autor");
 
@@ -25,45 +26,78 @@ public class MasterMindo {
             println("¡Se ha creado tu usuario!\n" +
                     "\nPresiona 1 para JUGAR como CODEMAKER o 2 para JUGAR com CODEBREAKER");
             int rol = scan.nextInt();
-            if(rol == 1) juegaCodeMaker();
-            else if(rol == 2) juegaCodeBreaker();
-            usr.finalizarPartidaActual();
+            if(rol == 1) juega(true);
+            else if(rol == 2) juega(false);
+
             println("El juego ha terminado.");
+    }
+
+
+    private static void juega(Boolean rol) {
+        println("Dame un nivel de dificultad: Facil, Medio y Dificil");
+        String diff = scan.next();
+        usr.creaPartidaActual(rol,diff);
+        pary = usr.getPartidaActual();
+        if (rol) juegaCodeBreaker();
+        else juegaCodeMaker();
     }
 
     /**
      * Permite al usuario jugar como CodeBreaker.
      */
     private static void juegaCodeBreaker() {
-        println("Dame un nivel de dificultad: Facil, Medio y Dificil");
-        Scanner scan = new Scanner(System.in);
-        String diff = scan.next();
-        usr.creaPartidaActual(false,diff);
-        pary = usr.getPartidaActual();
-        pary.generaCodigoSecretoAleatorio();
+        println("Partida empezada.");
         println("Intenta adivinar el código secreto.");
-        while(pary.getNumeroFilaActual() != 15 && !pary.isGanado()){
-            println("Introduce un código de tamaño " + pary.getNumColumnas() +" separado por espacios\n" +
-                    "Formado por numeros del 1 al "+ pary.getNumColores());
-            long startTime = System.currentTimeMillis();
+        int n;
+        Boolean abandonada = false;
+        while(pary.getNumeroFilaActual() != 15 && !pary.isGanado() && !abandonada){
+            println("¿Qué quieres hacer?");
+            println("[1] Introducir un nuevo intento.\n[2] Pedir una pista.\n" +
+                    "[3] Abandonar la partida.\n.");
+            while((n=scan.nextInt()) != 1) {
+                switch (n) {
+                    case 2:
+                        obtenerPista();
+                        break;
+                    case 3:
+                        println("¿Estás seguro de querer abandonar? Responde: 'Si' / 'No'.");
+                        if (scan.next().equals("Si")) abandonada = true;
+                        else break;
+                    default:
+                        println("¿Qué quieres hacer?");
+                        println("[1] Introducir un nuevo intento.\n[2] Pedir una pista.\n" +
+                                "[3] Abandonar la partida.\n[4] Reiniciar la partida.");
+                        break;
+                }
+            }
+            if (!abandonada) {
+                println("Introduce un código de tamaño " + pary.getNumColumnas() + " separado por espacios\n" +
+                        "Formado por numeros del 1 al " + pary.getNumColores());
+                long startTime = System.currentTimeMillis();
 
-            computarIntentoUsuario();
+                computarIntentoUsuario();
 
-            long endTime = System.currentTimeMillis();
-            pary.sumaTiempo(endTime - startTime);
+                long endTime = System.currentTimeMillis();
+                pary.sumaTiempo(endTime - startTime);
 
-            println("Has obtenido esta respuesta: " + pary.getUltimaRespuesta().toString());
+                println("Has obtenido esta respuesta: " + pary.getUltimaRespuesta().toString());
 
-            //todo falta añadir el uso de pistas
+                //todo falta añadir el uso de pistas
+            }
         }
+
         if (!pary.isGanado()) {
             println("Has perdido! Este era el código secreto:");
             System.out.println(pary.getCodigoSecreto().codigo);
+            if (abandonada) {
+                usr.abandonaPartidaActual();
+                return;
+            }
         }
-
         else {
             println("Has ganado con "+pary.getNumeroFilaActual()+" intentos.");
         }
+        usr.finalizarPartidaActual(pary.isGanado());
 
         println(usr.getNombre() + " tu puntuación total es de: " + pary.generaPuntuacion());
 
@@ -127,12 +161,16 @@ public class MasterMindo {
             try {
                 pary.setRespuesta(respuestaUsr);
                 break;
-            } catch (Exception e) {
-                println("Respuesta no válida. Comprueba tu respuesta.");
+            } catch (ExcepcionRespuestaIncorrecta e) {
+                println(e.getMessage());
             }
         }
     }
 
+
+    private static void obtenerPista() {
+
+    }
 
     /**
      * Lee el intento del usuario, lo añade al tablero y genera la respuesta.
