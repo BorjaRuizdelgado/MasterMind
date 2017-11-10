6c02777915dcbdd273e9e8f5bb676a4d0fef20c4
Isabel Codina <isakody@gmail.com>
2017-11-10 01:22:32 +0100

    ExcepcionPrueba añadida pero no implementada

---
+++ src/Domain/Excepciones/ExcepcionUno.java
@@ -0,0 +1,10 @@
+package Domain.Excepciones;
+
+
+//todo cambiar el nombre jeje esto es una prueba nomás
+
+public class ExcepcionUno extends Exception {
+    public ExcepcionUno (String message) {
+        super(message);
+    }
+}
