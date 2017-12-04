93e13d1439b20e43145275cc8b5be7f8fb5c9902
Isabel Codina <isakody@gmail.com>
2017-12-04 10:35:56 +0100

    GestionUsuario

--- src/Data/GestionUsuario.java
+++ src/Data/GestionUsuario.java
@@ -2,6 +2,7 @@ package Data;
 
 import Domain.Usuario;
 
+import java.io.*;
 import java.util.Map;
 
 public class GestionUsuario {
@@ -21,22 +22,44 @@ public class GestionUsuario {
     }
 
     private void cargarFinder(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(path);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            finder = (Map<String, Usuario>) objectInputStream.readObject();
+
+            objectInputStream.close();
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     private void guardarFinder(){
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(finder);
+            oos.close();
+            fos.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Usuario cargar(String id){
-        return null;
+        return finder.get(id);
     }
 
     public void guardar(Usuario usuario){
-
+        finder.put(usuario.getNombre(),usuario);
     }
 
     public boolean exists(String usuario){
-        return false;
+        return finder.containsKey(usuario);
     }
 }
