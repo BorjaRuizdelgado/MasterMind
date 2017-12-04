c81532223ea77556b66ab88601b430d8b6294b8f
Isabel Codina <isakody@gmail.com>
2017-12-04 10:35:46 +0100

    Borja

--- src/GUI/PantallaIni.java
+++ src/GUI/PantallaIni.java
@@ -4,4 +4,9 @@ import javax.swing.*;
 
 public class PantallaIni {
     private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
 }
