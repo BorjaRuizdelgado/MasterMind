77fbab3d0dc87b691c0548aad419bc29818db7d9
Isabel Codina <isakody@gmail.com>
2017-12-07 18:37:40 +0100

    Añadidos iconos para posible futuro uso y prueba de vista

---
+++ src/Presentation/PruebaIsa.java
@@ -0,0 +1,12 @@
+package Presentation;
+
+import javax.swing.*;
+
+public class PruebaIsa {
+    private JButton crearUsuarioButton;
+    private JButton cargarUsuarioButton;
+    private JButton ayudaButton;
+    private JButton rankingButton;
+    private JButton configuracionButton;
+    private JTextArea MASTERMINDOTextArea;
+}
