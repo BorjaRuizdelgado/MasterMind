6d12b681d5c4618e22fb05081942e87ff149523b
Omar Garcia <omar.anibal.garcia@est.fib.upc.edu>
2018-01-05 18:38:23 +0100

    Comentarios GestionSistemaRanking

--- src/Data/GestionSistemaRanking.java
+++ src/Data/GestionSistemaRanking.java
@@ -4,25 +4,44 @@ import Domain.SistemaRanking;
 
 import java.io.*;
 
+/**
+ * Clase GestionSistemRanking
+ * Gestor de la capa de datos que gestiona el ranking
+ * @author Omar
+ */
 public class GestionSistemaRanking {
     private String path;
     private static GestionSistemaRanking uniqueInstance;
 
+    /**
+     * Creadora GestionSistemaRanking
+     * Inicia el path donde se guarda el ranking
+     */
     private GestionSistemaRanking(){
         path = System.getProperty("user.dir") + "/Data/GSR/GestionSistemaRanking.obj";
     }
 
+    /**
+     * Obtener la instancia única de GestionSistemaRanking
+     * @return instancia unica
+     */
     public static GestionSistemaRanking getInstance() {
         if(uniqueInstance == null)
             uniqueInstance = new GestionSistemaRanking();
         return uniqueInstance;
     }
 
+    /**
+     * Crea el directorio donde se guarda el ranking.
+     */
     private void createDirectory(){
         File folder = new File(System.getProperty("user.dir") + "/Data/GSR");
         folder.mkdirs();
     }
 
+    /**
+     * Crea el archivo que guarda el ranking si no existe.
+     */
     private void createFile(){
         File file = new File(path);
         try {
@@ -33,6 +52,9 @@ public class GestionSistemaRanking {
         }
     }
 
+    /**
+     * Guarda el ranking en el directorio que contiene la clase.
+     */
     public void guardar(){
         SistemaRanking sistemaRanking = SistemaRanking.getInstance();
 
@@ -53,6 +75,9 @@ public class GestionSistemaRanking {
         }
     }
 
+    /**
+     * Carga sistemaRanking con los datos que contiene el archivo que tenemos apuntado en la clase como atributo.
+     */
     public void cargar(){
         try {
             FileInputStream fileInputStream = new FileInputStream(path);
